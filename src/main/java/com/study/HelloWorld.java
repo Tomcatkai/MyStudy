6f40aac2b86402136ce9a70ea423ba250aae0de7
Tomcatkai <tomcatkai@gmail.com>
2018-06-12 17:36:30 +0800

    新建Java练习项目

---
+++ src/main/java/com/study/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.study;
+
+/**
+ * HelloWorld
+ *
+ * @author kaizhang
+ * @create 2018-06-12 17:25
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
